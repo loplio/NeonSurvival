82b4399b5f9c2cc0844f8caf8dfbc152154bcd9b
75xor <75520201+75xor@users.noreply.github.com>
2023-05-15 03:45:43 +0900

    delete serverScene

--- NeonSurvival/Components_Neon.h
+++ NeonSurvival/Components_Neon.h
@@ -136,33 +136,3 @@ public:
 };
 
 
-//-------------------------------------------------------------------------------
-/*	SceneServer													   */
-//-------------------------------------------------------------------------------
-class SceneServer_Neon : public CScene {
-public:
-	SceneServer_Neon(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
-	virtual ~SceneServer_Neon() {};
-
-	void CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList) override;
-	void UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList) override;
-	void ReleaseShaderVariables() override;
-
-	// Build..
-	void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList) override;
-	void BuildLightsAndMaterials() override;
-	void ReleaseUploadBuffers() override;
-	void ReleaseObjects() override;
-
-	// ProcessInput..
-	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam) override;
-	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam) override;
-
-	// ProcessAnimaiton..
-	void AnimateObjects(float fTimeElapsed) override;
-
-	// ProcessOutput..
-	void OnPrepareRender(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera) override;
-	void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera) override;
-	void DrawUI(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera) override;
-};
