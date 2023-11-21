6d063cdeb1a492058808636ceabec2242b1abbe9
minwoo2500 <42472602+Yoon0511@users.noreply.github.com>
2023-11-21 20:06:13 +0900

    ip주소입력

--- NeonSurvival/Server.cpp
+++ NeonSurvival/Server.cpp
@@ -22,10 +22,12 @@ void SERVER::init(HWND hWnd)
     ZeroMemory(&serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(SERVERPORT);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+   // serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     //serverAddr.sin_addr.s_addr = inet_addr("125.180.24.40");
     //serverAddr.sin_addr.s_addr = inet_addr("119.67.181.24");
-    //serverAddr.sin_addr.s_addr = inet_addr("192.168.0.5");
+    char ip[14];
+    scanf("%s", ip);
+    serverAddr.sin_addr.s_addr = inet_addr(ip);
     //serverAddr.sin_addr.s_addr = inet_addr("125.180.29.106");
 
     //네이클 알고리즘 OFF
